844bc0e21b714e7b862a09246b4ce37100eb8660
Ivan <iegudyrev@gmail.com>
2024-04-24 14:16:09 +0300

    start 6.2.13

--- 6.2.12 class CyclicList.py
+++ 6.2.12 class CyclicList.py
@@ -50,9 +50,33 @@ class CyclicList:
         del self.it[indx]
         return el
         
-cyclic_list = CyclicList([1, 2, 3])
 
-for index, elem in enumerate(cyclic_list):
-    if index > 6:
-        break
-    print(elem, end=' ')
+# region foreign_solution
+
+
+from itertools import cycle
+
+
+# class CyclicList:
+#     def __init__(self, iterable=()):
+#         self._data = list(iterable) or []
+
+#     def append(self, item):
+#         self._data.append(item)
+
+#     def pop(self, index=None):
+#         if index is None:
+#             return self._data.pop()
+#         return self._data.pop(index)
+
+#     def __len__(self):
+#         return len(self._data)
+
+#     def __iter__(self):
+#         yield from cycle(self._data)
+
+#     def __getitem__(self, index):
+#         return self._data[index % len(self._data)]
+
+# endregion
+
